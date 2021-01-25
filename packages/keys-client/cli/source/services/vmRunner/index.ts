abf36e572e04eb93d488cd9c69ebc9d3d2ffbb17
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-01-25 16:36:09 +0200

    keys-client/cli > dev: vmRunner

--- packages/keys-client/cli/source/services/vmRunner/index.ts
+++ packages/keys-client/cli/source/services/vmRunner/index.ts
@@ -1,19 +1,28 @@
 // #region imports
     // #region libraries
     import vm from 'vm';
+
+    import http from 'http';
     // #endregion libraries
 // #endregion imports
 
 
 
 // #region module
+const baseSandbox = {
+    http,
+};
+
 const vmRunner = (
     code: string,
     sandbox: any,
 ) => {
     const obj = vm.runInNewContext(
         code,
-        sandbox,
+        {
+            ...sandbox,
+            ...baseSandbox,
+        },
         {
             timeout: 30,
             // https://nodejs.org/api/vm.html#vm_timeout_interactions_with_asynchronous_tasks_and_promises
